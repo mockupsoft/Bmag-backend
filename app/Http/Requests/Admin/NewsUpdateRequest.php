b2366be3c5b26546d0ce4b5cc83d6fb374a2fcd3
muharremozdemir <ozdemir.muharrem34@gmail.com>
2025-08-01 15:54:27 +0300

    yönetim paneli düzenlemeleri ve dikey dizi

--- app/Http/Requests/Admin/NewsUpdateRequest.php
+++ app/Http/Requests/Admin/NewsUpdateRequest.php
@@ -20,9 +20,11 @@ class NewsUpdateRequest extends FormRequest
             'content' => 'required|string',
             'category_id' => 'required|integer|exists:categories,id',
             'magazine_id' => 'required|integer|exists:magazines,id',
-            'large_image' => 'nullable',
-            'news_detail_image' => 'nullable',
-            'list_image' => 'nullable',
+
+            'large_image' => 'nullable|image|mimes:jpg,jpeg,png,webp|max:5120',
+            'news_detail_image' => 'nullable|image|mimes:jpg,jpeg,png,webp||max:2048',
+            'list_image' => 'nullable|image|mimes:jpg,jpeg,png,webp||max:2048',
+
             'tags' => 'nullable'
         ];
     }
@@ -35,10 +37,23 @@ class NewsUpdateRequest extends FormRequest
             'summary.required' => 'Özet alanı zorunludur.',
             'content.required' => 'İçerik alanı zorunludur.',
             'category_id.required' => 'Kategori alanı zorunludur.',
-            'magazine_id.required' => 'Degi alanı zorunludur.',
-            'large_image.required' => 'Resim alanı zorunludur.',
-            'news_detail_image.required' => 'Resim alanı zorunludur.',
-            'list_image.required' => 'Resim alanı zorunludur.',
+            'magazine_id.required' => 'Dergi alanı zorunludur.',
+
+            'large_image.image' => 'Büyük görsel geçerli bir resim dosyası olmalıdır.',
+            'large_image.mimes' => 'Büyük görsel jpg, jpeg, png veya webp formatında olmalıdır.',
+            'large_image.dimensions' => 'Büyük görselin boyutu 1300x720 piksel olmalıdır.',
+            'large_image.max' => 'Büyük görsel en fazla 5MB olabilir.',
+
+            'news_detail_image.image' => 'Haber detay görseli geçerli bir resim dosyası olmalıdır.',
+            'news_detail_image.mimes' => 'Haber detay görseli jpg, jpeg, png veya webp formatında olmalıdır.',
+            'news_detail_image.dimensions' => 'Haber detay görseli 650x360 piksel olmalıdır.',
+            'news_detail_image.max' => 'Haber detay görseli en fazla 2MB olabilir.',
+
+            'list_image.image' => 'Liste görseli geçerli bir resim dosyası olmalıdır.',
+            'list_image.mimes' => 'Liste görseli jpg, jpeg, png veya webp formatında olmalıdır.',
+            'list_image.dimensions' => 'Liste görseli 426x240 piksel olmalıdır.',
+            'list_image.max' => 'Liste görseli en fazla 2MB olabilir.',
         ];
     }
+
 }
